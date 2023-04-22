36858cca02537b986fdc341d02c4efe1ce46bf5f
Konstantin Nikolaenko <nikolaenko.kostya.vuz@mail.ru>
2023-04-22 16:20:29 +0300

    Init project

---
+++ src/test/java/com/app/server/era/EraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.server.era;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
